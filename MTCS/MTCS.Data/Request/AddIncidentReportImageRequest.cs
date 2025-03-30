7eca8a23240480fc2d0c25fb9a8738b720c2b109
HienMinh56 <dlhminh0506@gmail.com>
2025-03-30 23:33:45 +0700

    Update: Add Image API for IncidentReport

---
+++ MTCS/MTCS.Data/Request/AddIncidentReportImageRequest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCS.Data.Request
+{
+    public class AddIncidentReportImageRequest
+    {
+        [Required(ErrorMessage = "ReportId is required.")]
+        [StringLength(50, ErrorMessage = "ReportId must not exceed 50 characters.")]
+        public string ReportId { get; set; }
+
+        public int? ImageType { get; set; }
+
+        [MaxLength(5, ErrorMessage = "A maximum of 5 images are allowed.")]
+        public IFormFileCollection? Image { get; set; }
+    }
+}
