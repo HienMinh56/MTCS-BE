96aab00013a81b1f9f125f94539625b7581d7019
NguyenDucLinh411 <linhnguyen41103@gmail.com>
2025-04-19 14:21:49 +0700

    Incident History

---
+++ MTCS/MTCS.Data/DTOs/IncidentReportAdminDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCS.Data.DTOs
+{
+    public class IncidentReportAdminDTO
+    {
+        public string ReportId { get; set; }
+        public string TripId { get; set; }
+
+        public string IncidentType { get; set; }
+        public string Description { get; set; }
+        public DateTime IncidentTime { get; set; }
+
+        public string Status { get; set; }
+        public string? ResolutionDetails { get; set; }
+        public string? HandledBy { get; set; }
+        public DateTime? HandledTime { get; set; }
+
+        public string ReportedBy { get; set; }
+    }
+}
