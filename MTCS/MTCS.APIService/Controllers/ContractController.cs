13705b802cd3f4ff9aa85c3604fd356c76f282bd
TamMinhLinhTinh <mtam7896@gmail.com>
2025-04-22 00:13:06 +0700

    fix: auth in contract

--- MTCS/MTCS.APIService/Controllers/ContractController.cs
+++ MTCS/MTCS.APIService/Controllers/ContractController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MTCS.Common;
 using MTCS.Data.Request;
