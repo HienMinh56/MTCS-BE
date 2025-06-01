c78a511cc8600cbd32c9677bbdec181bfdfffc5e
VanMinh15 <minhcangthang@gmail.com>
2025-06-02 00:36:44 +0700

    Fix overall & customer revenue, trip financials, finance dashboard with expenses & incident cost

--- MTCS/MTCS.Data/Helpers/Pagination.cs
+++ MTCS/MTCS.Data/Helpers/Pagination.cs
@@ -44,5 +44,17 @@ namespace MTCS.Data.Helpers
 
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
+
+        public static PagedList<T> CreateFromList(
+    List<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count;
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
